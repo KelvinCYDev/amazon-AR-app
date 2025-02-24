b83a2127815425274c5d7da39e3876f753476c6a
KelvinCYDev <kelvincy.dev@gmail.com>
2025-02-23 16:10:06 -0800

    Add: Profile page when not logged in

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,5 +1,40 @@
-import { Text } from "tamagui";
-
+import { ProfileUnauthedBanner } from "@/components/Screens/profile/ProfileUnauthedBanner";
+import DefaultButton from "@/components/Shared/DefaultButton";
+import { router } from "expo-router";
+import {
+  Avatar,
+  Button,
+  Image,
+  ScrollView,
+  Sheet,
+  Text,
+  XStack,
+  YStack,
+} from "tamagui";
 export default function Profile() {
-  return <Text>Profile</Text>;
+  const session = false;
+  const onClickAuth = () => router.push("/login");
+  return (
+    <ScrollView bg={"white"}>
+      {session ? (
+        // Signed in components
+        <XStack jc="space-between" p={20} gap={20}></XStack>
+      ) : (
+        <YStack f={1} pt={40} ai={"center"} gap={45}>
+          <YStack w={"100%"} jc={"center"} ai={"center"} gap={40}>
+            <Text ta={"center"} fos={24}>
+              Sign in for the optimal experience
+            </Text>
+            <YStack w={"90%"} gap={15}>
+              <DefaultButton onPress={onClickAuth}>Sign In</DefaultButton>
+              <DefaultButton onPress={onClickAuth} variant="secondary">
+                Create Account
+              </DefaultButton>
+            </YStack>
+          </YStack>
+          <ProfileUnauthedBanner />
+        </YStack>
+      )}
+    </ScrollView>
+  );
 }
