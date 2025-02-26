60685eb0c812afbcc49ca186c6671f63f515648f
KelvinCYDev <kelvincy.dev@gmail.com>
2025-02-26 15:51:26 -0800

    Add: Fetch products from Supabase & display

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,22 @@
 import { HomeCarousel } from "@/components/Screens/home/HomeCarousel";
 import { HomeSuggestions } from "@/components/Screens/home/HomeSuggestions";
+import { ProductDealCard } from "@/components/Screens/home/ProductDealCard";
 import DefaultButton from "@/components/Shared/DefaultButton";
 import { DeliveryLocation } from "@/components/Shared/DeliveryLocation";
 import { HeaderTabsProps } from "@/components/Shared/header/HeaderTabs";
 import { useAuth } from "@/context/AuthProvider";
+import { supabase } from "@/supabase";
+import { Product } from "@/types/product";
 import { router, useNavigation } from "expo-router";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Alert } from "react-native";
-import { Text, ScrollView, YStack } from "tamagui";
+import { Text, ScrollView, YStack, XStack } from "tamagui";
 
 export default function Home() {
   const navigation = useNavigation();
   const { session } = useAuth();
   const onClickAuth = () => router.push("/login");
+  const [deals, setDeals] = useState<Product[]>([]);
 
   const tabs: HeaderTabsProps["tabs"] = [
     {
@@ -30,12 +34,27 @@ export default function Home() {
     },
   ];
 
+  const getDeals = useCallback(async () => {
+    try {
+      const { data = [] } = await supabase.from("products").select("*");
+      setDeals(data as Product[]);
+    } catch (error) {
+      console.log("error", error);
+    }
+  }, []);
+
+  const onProductPress = ({ id }: Product) => {
+    router.push(`/product/${id}`);
+  };
+
   useEffect(() => {
     navigation.setOptions({
       headerSearchShown: true,
       headerTabsProps: { tabs },
     });
+    getDeals();
   }, [navigation.setOptions]);
+
   return (
     <ScrollView f={1}>
       <DeliveryLocation />
@@ -46,7 +65,15 @@ export default function Home() {
           {session ? "Deals for you" : "Sign in for your best experience"}
         </Text>
         {session ? (
-          <></>
+          <XStack gap={30} jc={"space-between"} fw={"wrap"}>
+            {deals.map((product) => (
+              <ProductDealCard
+                key={product.id}
+                product={product}
+                onPress={() => onProductPress(product)}
+              />
+            ))}
+          </XStack>
         ) : (
           <DefaultButton onPress={onClickAuth}>Sign in Securely</DefaultButton>
         )}
