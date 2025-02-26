d76dd5a9b28189d1085a4404bdd29ff8f2705742
KelvinCYDev <kelvincy.dev@gmail.com>
2025-02-26 00:18:41 -0800

    Add: Auth with Supabase, eas config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,22 +3,25 @@ import { StatusBar } from "expo-status-bar";
 import { TamaguiProvider } from "tamagui";
 import { tamaguiConfig } from "../tamagui.config";
 import { Header } from "@/components/Shared/header/Header";
+import { AuthProvider } from "@/context/AuthProvider";
 
 export default function RootLayout() {
   return (
-    <TamaguiProvider config={tamaguiConfig}>
-      <StatusBar style="auto" />
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen
-          name="login"
-          options={{
-            headerShown: true,
-            header: (props) => <Header {...props} />,
-            presentation: "fullScreenModal",
-          }}
-        />
-      </Stack>
-    </TamaguiProvider>
+    <AuthProvider>
+      <TamaguiProvider config={tamaguiConfig}>
+        <StatusBar style="auto" />
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen
+            name="login"
+            options={{
+              headerShown: true,
+              header: (props) => <Header {...props} />,
+              presentation: "fullScreenModal",
+            }}
+          />
+        </Stack>
+      </TamaguiProvider>
+    </AuthProvider>
   );
 }
---
+++ context/AuthProvider.tsx
@@ -0,0 +1,41 @@
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { Session } from "@supabase/supabase-js";
+import { supabase } from "@/supabase";
+import { router, SplashScreen } from "expo-router";
+import { AppState } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
+
+const AuthContext = createContext({ session: null as Session | null });
+
+//if the app is in the foreground or background
+AppState.addEventListener("change", (state) => {
+  if (state === "active") supabase.auth.startAutoRefresh();
+  else supabase.auth.stopAutoRefresh();
+});
+
+export const AuthProvider = ({ children }: PropsWithChildren) => {
+  const [session, setSession] = useState<Session | null>(null);
+  useEffect(() => {
+    supabase.auth
+      .getSession()
+      .then(({ data: { session } }) => setSession(session));
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    router.replace("/(tabs)");
+    setTimeout(() => SplashScreen.hideAsync(), 1000);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ session }}>{children}</AuthContext.Provider>
+  );
+};
