4ef65ef52463e74ca08c6ef2510e83ae9ef69256
KelvinCYDev <kelvincy.dev@gmail.com>
2025-02-26 23:06:51 -0800

    Add: Integrate 3D model renderring

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,27 +5,30 @@ import { tamaguiConfig } from "../tamagui.config";
 import { Header } from "@/components/Shared/header/Header";
 import { AuthProvider } from "@/context/AuthProvider";
 import { CartProvider } from "@/context/CartProvider";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <CartProvider>
-        <TamaguiProvider config={tamaguiConfig}>
-          <StatusBar style="auto" />
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="(tabs)" />
-            <Stack.Screen name="(search)" />
-            <Stack.Screen
-              name="login"
-              options={{
-                headerShown: true,
-                header: (props) => <Header {...props} />,
-                presentation: "fullScreenModal",
-              }}
-            />
-          </Stack>
-        </TamaguiProvider>
-      </CartProvider>
-    </AuthProvider>
+    <GestureHandlerRootView>
+      <AuthProvider>
+        <CartProvider>
+          <TamaguiProvider config={tamaguiConfig}>
+            <StatusBar style="auto" />
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="(tabs)" />
+              <Stack.Screen name="(search)" />
+              <Stack.Screen
+                name="login"
+                options={{
+                  headerShown: true,
+                  header: (props) => <Header {...props} />,
+                  presentation: "fullScreenModal",
+                }}
+              />
+            </Stack>
+          </TamaguiProvider>
+        </CartProvider>
+      </AuthProvider>
+    </GestureHandlerRootView>
   );
 }
